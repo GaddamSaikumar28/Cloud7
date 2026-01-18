052fffb72c877c5988a21ca4902337d4ee02ec2e
saikumar <gaddamsaikumar12345@gmail.com>
2026-01-18 18:05:46 +0530

    bug fixes

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/layout/Navbar';
 import Footer from '../components/layout/Footer';
-// import { products } from '../data/mockData';
-// import ChatAssistant from '../components/ai/ChatAssistant';
+
 const RootLayout = () => {
   return (
     <div className="min-h-screen bg-dark-900 selection:bg-white/20 flex flex-col">
